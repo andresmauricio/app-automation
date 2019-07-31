9ecd677db02b04f763d899a0b26bd493539e3e5a
andres <andresacelas@outlook.com>
2019-07-31 15:21:59 -0500

    Add styles form login..

--- control/views.py
+++ control/views.py
@@ -1,8 +1,12 @@
 
-from django.http import HttpResponse  
+from django.http import HttpResponse 
+
+from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render, redirect
 
+
+@login_required
 def dashboard_view(request):
     
     return render(request, 'control/dashboard.html')
